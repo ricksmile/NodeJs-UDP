10cff2baf1007f3185142e85200220b8c0fcdd37
ricksmile <o_dormant@hotmail.com>
2015-04-13 16:14:39 +0800

    google compute Engine

---
+++ udpClient.js
@@ -0,0 +1,21 @@
+var dgram=require('dgram');
+var message=new Buffer('您好');
+var client=dgram.createSocket('udp4');
+client.send(message,0,message.length,5000,'localhost',function(err,bytes)
+{
+  if (err) {
+       console.log('發送數據失敗');
+  }
+  else console.log('已發送%d 字節數據',bytes);
+  
+  
+});
+
+client.on('message',function(msg,rinfo){
+  
+  console.log('已接受服務器端發送的數據:%s',msg);
+  console.log('服務器地址為%s',rinfo.address);
+  console.log('服務器PORT:%s',rinfo.port)
+});
+    
+    
---
+++ udpServer.js
@@ -0,0 +1,20 @@
+var dgram=require("dgram");
+var server =dgram.createSocket('udp4');
+//var address=server.address();
+server.on('message',function(msg,rinfo){
+    console.log('已接受客戶端發送的數據'+msg);
+    console.log('客戶端接受訊息為 %j'+msg);
+    var buf= new Buffer('確認訊息:'+msg);
+    server.send(buf,0,buf.length,rinfo.port,rinfo.address);
+    
+    });
+    
+    
+    server.on('listening',function() {
+     var address=server.address();
+      console.log('服務器開始監聽,地址訊息為 %j',address);        
+    });
+    
+    server.bind(5000,'localhost');
+    
+    
